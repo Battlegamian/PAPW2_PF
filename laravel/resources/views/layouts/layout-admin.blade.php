619d3968e3c80aea2e70044cee9e2d027b1914bc
SamuelSJ98 <36280541+SamuelSJ98@users.noreply.github.com>
2018-05-30 13:02:00 -0500

    Entrega final 004

--- laravel/resources/views/layouts/layout-admin.blade.php
+++ laravel/resources/views/layouts/layout-admin.blade.php
@@ -6,7 +6,7 @@
 		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 
-		<link rel='icon' href='images/icon.ico' type='image/x-icon' />
+		<link rel='icon' href="{{ URL::asset('images/icon.ico') }}" type='image/x-icon' />
 		<link rel='stylesheet' type='text/css' href='css/bootstrap.min.css'>
 		<link rel='stylesheet' type='text/css' href='icomoon/style.css'>
 		<link rel='stylesheet' type='text/css' href='css/main.css'>
--- laravel/resources/views/layouts/layout-channel.blade.php
+++ laravel/resources/views/layouts/layout-channel.blade.php
@@ -6,7 +6,7 @@
 		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
 		<meta name='viewport' content='width=device-width, initial-scale=1'>
 
-		<link rel='icon' href='images/icon.ico' type='image/x-icon' />
+		<link rel='icon' href="{{ URL::asset('images/icon.ico') }}" type='image/x-icon' />
 		<link rel='stylesheet' type='text/css' href="{{ URL::asset('css/bootstrap.min.css') }}">
 		<link rel='stylesheet' type='text/css' href="{{ URL::asset('icomoon/style.css') }}">
 		<link rel='stylesheet' type='text/css' href="{{ URL::asset('css/main.css') }}">
